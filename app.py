d8f1e6bb9831b68968a252f3207569f5534f88f3
Dave <100411475+CoderKuno@users.noreply.github.com>
2024-05-22 03:17:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 from datetime import datetime, timedelta
-from prophet import Prophet
-from prophet.plot import plot_plotly
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly
 from plotly import graph_objs as go
 import firebase_admin
 from firebase_admin import credentials, db
